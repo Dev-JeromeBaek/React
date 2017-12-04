2eb493d3e5aa31eb00c5a3d7c8bd852367cbbcf0
Dev-JermoeBaek <superior051@icloud.com>
2017-12-04 17:35:14 +0900

    .

--- project-cra-noeject/src/App.tsx
+++ project-cra-noeject/src/App.tsx
@@ -3,13 +3,33 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component<{}, null> {
+export interface AppProps {
+  name: string;
+}
+
+interface AppState {
+  age: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      age: 27
+    };
+    setInterval(() => {
+      this.setState({
+        age: this.state.age + 1
+      });
+    }, 2000);
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.props.name}, {this.state.age}</h2>
+          <StatelessComponent name="Baek">나는 JeromeBaek이다.</StatelessComponent>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
@@ -18,5 +38,11 @@ class App extends React.Component<{}, null> {
     );
   }
 }
+
+const StatelessComponent: React.SFC<AppProps> = (props) => {
+  return (
+    <h2>{props.name} {props.children}</h2>
+  );
+};
 
 export default App;
