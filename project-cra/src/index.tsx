a7481f67233e32e81347942aacaa7be94d215ae7
Dev-JermoeBaek <superior051@icloud.com>
2017-12-04 13:13:49 +0900

    .

--- project-cra/src/index.tsx
+++ project-cra/src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// react-dom 라이브러리를 이용해서 DOM 에 리액트 컴포넌트를 매칭
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
