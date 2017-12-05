ec3bfb166e673080847a9bc2a1cf5cb8419073a7
Dev-JermoeBaek <superior051@icloud.com>
2017-12-05 15:17:31 +0900

    .

--- project-cra-noeject/src/App.tsx
+++ project-cra-noeject/src/App.tsx
@@ -1,48 +1,89 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 export interface AppProps {
   name: string;
 }
 
-interface AppState {
+export interface AppState {
   age: number;
+  company: string;
 }
 
 class App extends React.Component<AppProps, AppState> {
+  private _interval: number;
+
   constructor(props: AppProps) {
+    console.log('App constructor');
     super(props);
     this.state = {
-      age: 27
+      age: 35,
+      company: 'Studio XID'
     };
-    setInterval(() => {
-      this.setState({
-        age: this.state.age + 1
-      });
-    }, 2000);
+    this._reset = this._reset.bind(this);
+    this._change = this._change.bind(this);
   }
+
+  componentWillMount() {
+    console.log('App componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('App componentDidMount');
+    this._interval = window.setInterval(
+      () => {
+        this.setState({
+          age: this.state.age + 1
+        });
+      },
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    console.log('App componentWillUnmount');
+    clearInterval(this._interval);
+  }
+
+  componentWillReceiveProps(nextProps: AppProps) {
+    console.log(`App componentWillReceiveProps : ${JSON.stringify(nextProps)}`);
+  }
+
+  shouldComponentUpdate(nextProps: AppProps, nextState: AppState): boolean {
+    console.log(`App shouldComponentUpdate : ${JSON.stringify(nextProps)}, ${JSON.stringify(nextState)}`);
+    return true;
+  }
+
+  componentWillUpdate(nextProps: AppProps, nextState: AppState) {
+    console.log(`App componentWillUpdate : ${JSON.stringify(nextProps)}, ${JSON.stringify(nextState)}`);
+  }
+
+  componentDidUpdate(prevProps: AppProps, prevState: AppState) {
+    console.log(`App componentDidUpdate : ${JSON.stringify(prevProps)}, ${JSON.stringify(prevState)}`);
+  }
+
   render() {
+    console.log('App render');
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.props.name}, {this.state.age}</h2>
-          <StatelessComponent name="Baek">나는 JeromeBaek이다.</StatelessComponent>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <h2>Hello {this.props.name} - {this.state.age}</h2>
+        <button onClick={this._reset}>리셋</button>
+        <input type="text" onChange={this._change} value={this.state.company} />
       </div>
     );
   }
-}
 
-const StatelessComponent: React.SFC<AppProps> = (props) => {
-  return (
-    <h2>{props.name} {props.children}</h2>
-  );
-};
+  private _reset(): void {
+    this.setState({
+      age: 35
+    });
+  }
+
+  private _change(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      company: e.target.value
+    });
+  }
+}
 
 export default App;
